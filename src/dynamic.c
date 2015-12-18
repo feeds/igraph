ee306b5c99a624d99701ace214db82702aff0447
Erik Scharwächter <erik.scharwaechter@gmail.com>
2015-12-18 13:10:38 +0100

    source files for dynamic graph mining

---
+++ src/dynamic.c
@@ -0,0 +1,38 @@
+/* vim:set ts=8 sw=2 sts=2 noet:  */
+/* 
+   Algorithms for dynamic graphs
+   Copyright (C) 2015  Erik Scharwaechter <erik.scharwaechter@rwth-aachen.de>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+   02110-1301 USA
+
+*/
+
+#include <stdlib.h>
+#include "igraph_dynamic.h"
+#include "igraph_interface.h"
+#include "igraph_memory.h"
+
+//#include <string.h> // memcpy
+//#include "igraph_fsm.h"
+//#include "igraph_matrix.h"
+//#include "igraph_stack.h"
+//#include "igraph_components.h"
+//#include "igraph_constructors.h"
+
+int igraph_read_dynamic_velist(igraph_vector_ptr_t *graphs, FILE *instream) {
+  return 0;
+}
+
